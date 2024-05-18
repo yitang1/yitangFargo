5df973bc45a3cd5e3b6882de57b0ebeef6bcbb4b
yitang1 <2039054446@qq.com>
2024-05-19 03:13:17 +0800

    寰宇之魂下级配方修改-收尾工作
    以下改动基于已开启【配方修改-适配灾法双开】这个模组配置的前提：
    
    1.【流浪者之魂】的合成配方中：
    移除 [终结者之刃]
    [辐毒废水] 替换为 [驱魔十字架]
    [星体暴乱] 替换为 [堕天星盘]
    [瘟疫注射器] 替换为 [孔雀翎]
    [掠星镰] 替换为 [始源病毒]
    [辞别飞镰] 替换为 [震波炸弹]
    [焦锅] 替换为 [日蚀之陨]
    添加 [死神遗镰]
    (个人从来没玩过盗贼职业，也不知道这样安排是否合理，
    但是已经尽量遵循配方设计原则去调整了🤕)
    
    2.移除【召唤之魂】合成配方中的 [海盗法杖]

--- Content/Items/Accessories/Souls/VagabondsSoulNew.cs
+++ Content/Items/Accessories/Souls/VagabondsSoulNew.cs
@@ -8,6 +8,7 @@ using FargowiltasCrossmod.Content.Calamity.Items.Accessories.Essences;
 using FargowiltasCrossmod.Content.Calamity.Items.Accessories.Souls;
 using FargowiltasSouls.Content.Items.Accessories.Souls;
 using FargowiltasSouls.Core.AccessoryEffectSystem;
+using CalamityMod.Items.Weapons.DraedonsArsenal;
 
 namespace yitangFargo.Content.Items.Accessories.Souls
 {
@@ -39,22 +40,23 @@ namespace yitangFargo.Content.Items.Accessories.Souls
         }
         public override void AddRecipes()
         {
-            //盗贼之魂默认为龙后时期
+            //盗贼的魂饰品默认为龙后时期
             CreateRecipe()
                 .AddIngredient<OutlawsEssence>()
                 .AddIngredient<EclipseMirror>()
                 .AddIngredient<Nanotech>()
-                .AddIngredient<DragonScales>()
                 .AddIngredient<VeneratedLocket>()
+                .AddIngredient<DragonScales>()
                 .AddIngredient<WaveSkipper>()
                 .AddIngredient<GraveGrimreaver>()
-                .AddIngredient<SpentFuelContainer>()
-                .AddIngredient<TheSyringe>()
+                .AddIngredient<Exorcism>()
+                .AddIngredient<HeavenfallenStardisk>()
+                .AddIngredient<Malachite>()
                 .AddIngredient<RegulusRiot>()
-                .AddIngredient<CelestialReaper>()
-                .AddIngredient<Valediction>()
-                .AddIngredient<ExecutionersBlade>()
-                .AddIngredient<SearedPan>()
+                .AddIngredient<AlphaVirus>()
+                .AddIngredient<WavePounder>()
+                .AddIngredient<TheOldReaper>()
+                .AddIngredient<EclipsesFall>()
                 .AddTile(ModContent.Find<ModTile>("Fargowiltas", "CrucibleCosmosSheet"))
                 .Register();
         }
