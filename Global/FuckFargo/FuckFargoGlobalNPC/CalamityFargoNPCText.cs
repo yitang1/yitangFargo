15ce4de5b6e7d0834613b9a792956f9f3c73d0a3
yitang1 <2039054446@qq.com>
2024-05-24 10:46:55 +0800

    更新版本：0.3.9.0
    对一些“非本地化”的文本进行了汉化。这包括：
    
    1.灾厄的城镇NPC——永冻大法师，在首次被击败和非首次被击败后的两条对话文本。
    
    2.Fargo的城镇NPC——戴薇安，其在开启灾法联动(Fargo's Souls DLC)模组时，
    击败灾厄Mod的一些稀有敌怪时解锁的状态讯息文本(戴薇安解锁了新商品的提示)；
    以及，灾法联动Mod给戴薇安添加的商店切换UI按钮的文本。
    
    (注：戴薇安的商店UI目前因为其对话框文本量的不同，会导致有时对话框会遮挡新UI的按钮。
    受个人水平限制，目前还未找到通过Mod去修改UI按钮位置的方法。😣)

---
+++ Global/FuckFargo/FuckFargoGlobalNPC/CalamityFargoNPCText.cs
@@ -0,0 +1,28 @@
+﻿using CalamityMod.NPCs.TownNPCs;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace yitangFargo.Global.FuckFargo.FuckFargoGlobalNPC
+{
+    public class CalamityFargoNPCText : GlobalNPC
+    {
+        public override void GetChat(NPC npc, ref string chat)
+        {
+            //大法师NPC
+            if (npc.type == ModContent.NPCType<DILF>())
+            {
+                switch (chat)
+                {
+                    case "You clearly possess great skill! My thanks to you for freeing me, and providing an opportunity to defrost my fighting abilities.":
+                        chat = "嗯，你显然有着高超的技术和实力！我由衷地感谢你解救了我，并为我提供了一个解除封印后伸展筋骨的机会。";
+                        break;
+                    case "Aah, a good round of sparring. I need the exercise, so thanks for that.":
+                        chat = "啊，打得很好，看来我自身还是有着不足之处，需要多加锻炼。总而言之，一切都已结束，谢谢你了。";
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Global/FuckFargo/FuckFargoSystem/CalamityFargoSystem.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.UI;
+using Terraria.UI.Chat;
+
+namespace yitangFargo.Global.FuckFargo.FuckFargoSystem
+{
+    public class CalamityFargoSystem : ModSystem
+    {
+        public override void Load()
+        {
+            On_ChatManager.ParseMessage += Translate;
+        }
+        public override void Unload()
+        {
+            On_ChatManager.ParseMessage -= Translate;
+        }
+
+        private static List<TextSnippet> Translate(On_ChatManager.orig_ParseMessage orig, string text, Color baseColor)
+        {
+            text = Regex.Replace(text, "A new item has been unlocked in Deviantt's shop!", "戴薇安解锁了新商品！");
+            text = Regex.Replace(text, "Switch Mod", "切换页面");
+            text = Regex.Replace(text, "Vanilla", "原版商店");
+            text = Regex.Replace(text, "Calamity", "灾厄商店");
+            return orig.Invoke(text, baseColor);
+        }
+    }
+}
