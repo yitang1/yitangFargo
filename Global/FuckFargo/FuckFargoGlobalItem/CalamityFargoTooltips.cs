f97b6c651a7c1f5b97395b5b0c138c06d99d5512
yitang1 <2039054446@qq.com>
2024-06-13 00:16:21 +0800

    修复一个关于魔石文本的bug

--- Global/FuckFargo/FuckFargoGlobalItem/CalamityFargoTooltips.cs
+++ Global/FuckFargo/FuckFargoGlobalItem/CalamityFargoTooltips.cs
@@ -72,31 +72,31 @@ namespace yitangFargo.Global.FuckFargo.FuckFargoGlobalItem
             #region 通用/其他
             //突变体的礼物
             if (item.type == ModContent.ItemType<Masochist>())
-        {
-            tooltips.Replace("[c/FF0000:Calamity Crossmod Support:] Disabled. Use Calamity's difficulty UI instead!", "已禁用\n请到灾厄的难度指示器UI里切换难度");
-        }
-        //终末石
-        if (item.type == ModContent.ItemType<Terminus>())
-        {
-            tooltips.Replace("[c/FF0000:Calamity Crossmod Support:] Can only be used after defeating the Mutant", "只能在击败突变体后使用");
-        }
-        //天体洋葱
-        if (item.type == ModContent.ItemType<CelestialOnion>())
-        {
-            tooltips.Replace("[c/FF0000:{BalanceLine}]Is now an upgrade to [i:{ModContent.ItemType<MutantsPact>()}]Mutant's Pact, that allows any accessory in the extra slot.", "");
-        }
-        //古恒石
-        if (item.type == ModContent.ItemType<Rock>())
-        {
-            tooltips.Replace("[c/AAAAAA:Sold by Squirrel]", "");
-        }
+            {
+                tooltips.Replace("[c/FF0000:Calamity Crossmod Support:] Disabled. Use Calamity's difficulty UI instead!", "已禁用\n请到灾厄的难度指示器UI里切换难度");
+            }
+            //终末石
+            if (item.type == ModContent.ItemType<Terminus>())
+            {
+                tooltips.Replace("[c/FF0000:Calamity Crossmod Support:] Can only be used after defeating the Mutant", "只能在击败突变体后使用");
+            }
+            //天体洋葱
+            if (item.type == ModContent.ItemType<CelestialOnion>())
+            {
+                tooltips.Replace("[c/FF0000:{BalanceLine}]Is now an upgrade to [i:{ModContent.ItemType<MutantsPact>()}]Mutant's Pact, that allows any accessory in the extra slot.", "");
+            }
+            //古恒石
+            if (item.type == ModContent.ItemType<Rock>())
+            {
+                tooltips.Replace("[c/AAAAAA:Sold by Squirrel]", "");
+            }
 
-        if (item.ModItem != null && item.ModItem is BaseEnchant)
-        {
-            tooltips.Replace(" ", "");
-            tooltips.Replace("]:", "] ");
-        }
-        #endregion
+            if (item.ModItem != null && item.ModItem is BaseEnchant)
+            {
+                tooltips.Replace(" [", "[");
+                tooltips.Replace("]:", "]");
+            }
+            #endregion
         }
     }
 }
