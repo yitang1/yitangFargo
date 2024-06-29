084405eeb9f4e93c26c94609fc76e08eb9197d1e
yitang1 <2039054446@qq.com>
2024-06-30 01:04:00 +0800

    微调一些汉化文本

--- Global/FuckFargo/FuckFargoGlobalNPC/CalamityFargoNPCText.cs
+++ Global/FuckFargo/FuckFargoGlobalNPC/CalamityFargoNPCText.cs
@@ -17,7 +17,7 @@ namespace yitangFargo.Global.FuckFargo.FuckFargoGlobalNPC
                 switch (chat)
                 {
                     case "You clearly possess great skill! My thanks to you for freeing me, and providing an opportunity to defrost my fighting abilities.":
-                        chat = "嗯，你显然有着高超的技术和实力！我由衷地感谢你解救了我，并为我提供了一个解除封印后伸展筋骨的机会。";
+                        chat = "嗯，你显然有着高超的技术和实力！我由衷地感谢你解救了我，而且还在刚刚给了我一个解除封印后伸展筋骨的机会。";
                         break;
                     case "Aah, a good round of sparring. I need the exercise, so thanks for that.":
                         chat = "啊……打得很好，看来我自身还是有着不足之处，需要多加锻炼。总而言之，一切都已结束，谢谢你了。";
