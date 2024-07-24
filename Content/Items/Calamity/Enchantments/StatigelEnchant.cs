62edcaa62ae45c0e8f1ea943ea4524f22c5f1d55
yitang1 <2039054446@qq.com>
2024-07-24 22:35:38 +0800

    添加完整的【斯塔提斯魔石】的效果

--- Content/Items/Calamity/Enchantments/StatigelEnchant.cs
+++ Content/Items/Calamity/Enchantments/StatigelEnchant.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
@@ -15,11 +15,12 @@ using FargowiltasSouls.Core.Toggler;
 using yitangFargo.Common;
 using yitangFargo.Common.Toggler;
 using yitangFargo.Content.Projectiles.Minions;
+using yitangFargo.Global.Config;
 
 namespace yitangFargo.Content.Items.Calamity.Enchantments
 {
-    public class StatigelEnchant : BaseEnchant
-    {
+    public class StatigelEnchant : BaseEnchant, ILocalizedModType
+	{
         public override Color nameColor => new(233, 107, 182);
 
         public override void SetStaticDefaults()
@@ -43,10 +44,21 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
             //斯塔提斯盔甲
             if (player.HasEffect<StatigelEffect>())
             {
-                player.Calamity().statigelSet = true;
-                player.GetJumpState<StatigelJump>().Enable();
-                Player.jumpHeight += 5;
-                player.jumpSpeedBoost += 0.6f;
+				if (!ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					player.Calamity().statigelSet = true;
+					player.GetJumpState<StatigelJump>().Enable();
+					Player.jumpHeight += 5;
+					player.jumpSpeedBoost += 0.6f;
+				}
+				else if (ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					ModContent.GetInstance<StatigelHeadMelee>().UpdateArmorSet(player);
+					ModContent.GetInstance<StatigelHeadRanged>().UpdateArmorSet(player);
+					ModContent.GetInstance<StatigelHeadMagic>().UpdateArmorSet(player);
+					ModContent.GetInstance<StatigelHeadSummon>().UpdateArmorSet(player);
+					ModContent.GetInstance<StatigelHeadRogue>().UpdateArmorSet(player);
+				}
             }
             //魔能谐振仪
             if (player.HasEffect<StatigelManaPolarizer>())
@@ -55,7 +67,21 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
             }
         }
 
-        public override void AddRecipes()
+		public override void SafeModifyTooltips(List<TooltipLine> tooltips)
+		{
+			base.SafeModifyTooltips(tooltips);
+
+			if (!ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[StatigelFullEffects]", "");
+			}
+			else if (ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[StatigelFullEffects]", this.GetLocalizedValue("StatigelFullTooltip"));
+			}
+		}
+
+		public override void AddRecipes()
         {
             CreateRecipe()
                 .AddRecipeGroup("yitangFargo:AnyStatigel")
