57d35f79985c3fc6e77faaebde2ad99046855a09
yitang1 <2039054446@qq.com>
2024-07-25 02:20:42 +0800

    添加完整的【钨钢魔石】(旧版)的效果

--- Content/Items/Calamity/Enchantments/WulfrumEnchant.cs
+++ Content/Items/Calamity/Enchantments/WulfrumEnchant.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
@@ -11,11 +11,14 @@ using FargowiltasSouls.Core.AccessoryEffectSystem;
 using FargowiltasSouls.Content.Items.Accessories.Enchantments;
 using yitangFargo.Common;
 using yitangFargo.Common.Toggler;
+using yitangFargo.Global.Config;
+using CalamityMod.CalPlayer;
+using CalamityMod;
 
 namespace yitangFargo.Content.Items.Calamity.Enchantments
 {
-    public class WulfrumEnchant : BaseEnchant
-    {
+    public class WulfrumEnchant : BaseEnchant, ILocalizedModType
+	{
         public override Color nameColor => new(163, 218, 129);
 
         public override void SetStaticDefaults()
@@ -36,14 +39,31 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
             player.AddEffect<WulfrumRoverDrive>(Item);
             player.AddEffect<WulfrumTrinketofChi>(Item);
 
-            //钨钢盔甲 (旧版本的通用盔甲属性)
-            if (player.HasEffect<WulfrumEffect>())
+			CalamityPlayer calamityPlayer = player.Calamity();
+			//钨钢盔甲 (旧版本的通用盔甲属性)
+			if (player.HasEffect<WulfrumEffect>())
             {
-                player.statDefense += 3;
-                if (player.statLife <= (int)(player.statLifeMax2 * 0.5))
-                {
-                    player.statDefense += 10;
-                }
+				if (!ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					player.statDefense += 3;
+					if (player.statLife <= (int)(player.statLifeMax2 * 0.5))
+					{
+						player.statDefense += 10;
+					}
+				}
+				else if (ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					player.aggro += 100;
+					player.slotsMinions++;
+					calamityPlayer.rogueStealthMax += 0.5f;
+					calamityPlayer.wearingRogueArmor = true;
+
+					player.statDefense += 3;
+					if (player.statLife <= (int)(player.statLifeMax2 * 0.5))
+					{
+						player.statDefense += 10;
+					}
+				}
             }
             //钨钢屏障生成仪
             if (player.HasEffect<WulfrumRoverDrive>())
@@ -57,7 +77,21 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
             }
         }
 
-        public override void AddRecipes()
+		public override void SafeModifyTooltips(List<TooltipLine> tooltips)
+		{
+			base.SafeModifyTooltips(tooltips);
+
+			if (!ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[WulfrumFullEffects]", "");
+			}
+			else if (ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[WulfrumFullEffects]", this.GetLocalizedValue("WulfrumFullTooltip"));
+			}
+		}
+
+		public override void AddRecipes()
         {
             CreateRecipe()
                 .AddIngredient<WulfrumHat>()
