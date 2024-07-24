688d4f14f36ff713a742ae019338d92364578e60
yitang1 <2039054446@qq.com>
2024-07-25 02:19:34 +0800

    添加完整的【泰坦之心魔石】的效果

--- Content/Items/Calamity/Enchantments/TitanHeartEnchant.cs
+++ Content/Items/Calamity/Enchantments/TitanHeartEnchant.cs
@@ -2,21 +2,24 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using CalamityMod;
 using CalamityMod.Items.Accessories;
 using CalamityMod.Items.Armor.TitanHeart;
 using CalamityMod.Tiles.Furniture.CraftingStations;
+using CalamityMod.Items.Fishing.AstralCatches;
+using CalamityMod.Items.Weapons.Rogue;
 using FargowiltasSouls.Core.Toggler;
 using FargowiltasSouls.Core.Toggler.Content;
 using FargowiltasSouls.Core.AccessoryEffectSystem;
 using FargowiltasSouls.Content.Items.Accessories.Enchantments;
 using yitangFargo.Common.Toggler;
-using CalamityMod.Items.Fishing.AstralCatches;
-using CalamityMod.Items.Weapons.Rogue;
+using yitangFargo.Global.Config;
+using yitangFargo.Common;
 
 namespace yitangFargo.Content.Items.Calamity.Enchantments
 {
-	public class TitanHeartEnchant : BaseEnchant
+	public class TitanHeartEnchant : BaseEnchant, ILocalizedModType
 	{
 		public override Color nameColor => new(140, 129, 187);
 
@@ -39,11 +42,32 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
 			//泰坦之心盔甲
 			if (player.HasEffect<AstralTitanHeartEffect>())
 			{
-				player.Calamity().titanHeartSet = true;
+				if (!ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					player.Calamity().titanHeartSet = true;
+				}
+				else if (ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					ModContent.GetInstance<TitanHeartMask>().UpdateArmorSet(player);
+				}
 			}
 			//大熊座雀鲷 (无降低移速的负面效果)
 			player.moveSpeed += 0.15f;
 			ModContent.GetInstance<UrsaSergeant>().UpdateAccessory(player, hideVisual);
+		}
+
+		public override void SafeModifyTooltips(List<TooltipLine> tooltips)
+		{
+			base.SafeModifyTooltips(tooltips);
+
+			if (!ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[TitanHeartFullEffects]", "");
+			}
+			else if (ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[TitanHeartFullEffects]", this.GetLocalizedValue("TitanHeartFullTooltip"));
+			}
 		}
 
 		public override void AddRecipes()
