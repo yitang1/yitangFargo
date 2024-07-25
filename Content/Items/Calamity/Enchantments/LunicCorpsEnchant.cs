d43fb32f6d3796980f4d0a032ad2896cc0a344ea
yitang1 <2039054446@qq.com>
2024-07-25 18:37:13 +0800

    添加完整的【雷神之锤魔石】的效果

--- Content/Items/Calamity/Enchantments/LunicCorpsEnchant.cs
+++ Content/Items/Calamity/Enchantments/LunicCorpsEnchant.cs
@@ -15,10 +15,12 @@ using FargowiltasSouls.Content.Items.Accessories.Enchantments;
 using yitangFargo.Common.Toggler;
 using yitangFargo.Common;
 using yitangFargo.Content.Buffs;
+using yitangFargo.Global.Config;
+using System.Collections.Generic;
 
 namespace yitangFargo.Content.Items.Calamity.Enchantments
 {
-	public class LunicCorpsEnchant : BaseEnchant
+	public class LunicCorpsEnchant : BaseEnchant, ILocalizedModType
 	{
 		public override Color nameColor => new(162, 191, 132);
 
@@ -40,12 +42,26 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
 			player.AddEffect<DLunicCorpsTheAbsorber>(Item);
 
 			//雷神之锤盔甲
+			yitangFargoPlayer yitangPlayer = player.yitangFargo();
 			if (player.HasEffect<DLunicCorpsEffect>())
 			{
-				yitangFargoPlayer yitangPlayer = player.yitangFargo();
-				if (yitangPlayer.LunicCorpsCooldown <= 0)
+				if (!ytFargoConfig.Instance.FullCalamityEnchant)
 				{
-					player.AddBuff(ModContent.BuffType<LunicCorpsShieldBuff>(), 2);
+					if (yitangPlayer.LunicCorpsCooldown <= 0)
+					{
+						player.AddBuff(ModContent.BuffType<LunicCorpsShieldBuff>(), 2);
+					}
+				}
+				else if (ytFargoConfig.Instance.FullCalamityEnchant)
+				{
+					ModContent.GetInstance<LunicCorpsHelmet>().UpdateArmorSet(player);
+					//if (yitangPlayer.LunicCorpsCooldown <= 0)
+					//{
+					//	player.AddBuff(ModContent.BuffType<LunicCorpsShieldBuff>(), 2);
+					//}
+					//player.bulletDamage += 0.1f;
+					//player.specialistDamage += 0.1f;
+					//player.jumpSpeedBoost += 1f;
 				}
 			}
 			//阴阳吸星石
@@ -55,6 +71,20 @@ namespace yitangFargo.Content.Items.Calamity.Enchantments
 			}
 			//老旧骰子
 			ModContent.GetInstance<OldDie>().UpdateAccessory(player, hideVisual);
+		}
+
+		public override void SafeModifyTooltips(List<TooltipLine> tooltips)
+		{
+			base.SafeModifyTooltips(tooltips);
+
+			if (!ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[LunicCorpsFullEffects]", "");
+			}
+			else if (ytFargoConfig.Instance.FullCalamityEnchant)
+			{
+				tooltips.ReplaceText("[LunicCorpsFullEffects]", this.GetLocalizedValue("LunicCorpsFullTooltip"));
+			}
 		}
 
 		public override void AddRecipes()
