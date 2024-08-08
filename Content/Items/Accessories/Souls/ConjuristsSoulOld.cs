69ae98d03cfcd8883288b419b2488809e912c36c
yitang1 <2039054446@qq.com>
2024-08-09 03:05:14 +0800

    添加旧版【召唤之魂】

---
+++ Content/Items/Accessories/Souls/ConjuristsSoulOld.cs
@@ -0,0 +1,78 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using FargowiltasSouls;
+using FargowiltasSouls.Content.Items.Accessories.Souls;
+using FargowiltasSouls.Content.Items.Accessories.Essences;
+using yitangFargo.Global.Config;
+using CalamityMod.Items.Weapons.Summon;
+
+namespace yitangFargo.Content.Items.Accessories.Souls
+{
+    public class ConjuristsSoulOld : BaseSoul
+    {
+        public static readonly Color ItemColor = new(0, 255, 255);
+        protected override Color? nameColor => ItemColor;
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.FargoSouls().SummonSoul = true;
+            player.GetDamage(DamageClass.Summon) += 0.3f;
+            player.GetCritChance(DamageClass.Summon) += 15;
+
+            player.GetAttackSpeed(DamageClass.SummonMeleeSpeed) += 0.2f;
+            player.whipRangeMultiplier += 0.15f;
+
+            player.GetKnockback(DamageClass.Summon) += 3f;
+
+            player.maxMinions += 5;
+            player.maxTurrets += 5;
+        }
+
+        public override void AddRecipes()
+        {
+            if (ytFargoConfig.Instance.FargoSoulsRecipe)
+            {
+                CreateRecipe()
+                    .AddIngredient<OccultistsEssence>()
+                    .AddIngredient(ItemID.MonkBelt)
+                    .AddIngredient(ItemID.SquireShield)
+                    .AddIngredient(ItemID.HuntressBuckler)
+                    .AddIngredient(ItemID.ApprenticeScarf)
+                    .AddIngredient(ItemID.PygmyNecklace)
+                    .AddIngredient(ItemID.PapyrusScarab)
+                    .AddIngredient(ItemID.Smolstar)
+                    .AddIngredient(ItemID.PirateStaff)
+                    .AddIngredient(ItemID.OpticStaff)
+                    .AddIngredient(ItemID.DeadlySphereStaff)
+                    .AddIngredient(ItemID.StormTigerStaff)
+                    .AddIngredient(ItemID.StaffoftheFrostHydra)
+                    .AddIngredient(ItemID.TempestStaff)
+                    .AddIngredient(ItemID.RavenStaff)
+                    .AddIngredient(ItemID.XenoStaff)
+                    .AddIngredient(ItemID.EmpressBlade)
+                    .AddTile(ModContent.Find<ModTile>("Fargowiltas", "CrucibleCosmosSheet"))
+                    .Register();
+            }
+
+            if (ytFargoConfig.Instance.CalamityFargoRecipe)
+            {
+                CreateRecipe()
+                    .AddIngredient<OccultistsEssence>()
+                    .AddIngredient(ItemID.ApprenticeScarf)
+                    .AddIngredient(ItemID.SquireShield)
+                    .AddIngredient(ItemID.MonkBelt)
+                    .AddIngredient(ItemID.HuntressBuckler)
+                    .AddIngredient(ItemID.PygmyNecklace)
+                    .AddIngredient(ItemID.PapyrusScarab)
+
+                    .AddIngredient<EtherealSubjugator>()
+                    .AddIngredient<GuidelightofOblivion>()
+                    .AddIngredient<EndoHydraStaff>()
+                    .AddTile(ModContent.Find<ModTile>("Fargowiltas", "CrucibleCosmosSheet"))
+                    .Register();
+            }
+        }
+    }
+}
